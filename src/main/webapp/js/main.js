5c2031ae58d71659a265c1d939d1670f421e6b29
s20212057 <s20212057@HCS19063.stdlan.hcs.ac.jp>
2022-11-11 11:39:09 +0900

    localstorageログアウト機能途中

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -5,3 +5,10 @@ $(window).on('load', function(){
 	//alert('Hello!!!!');
 	$("#logArea").scrollTop($("#logArea")[0].scrollHeight)
  });
+function logout(){
+	var loginData = {
+			mailAddress: $('#mailAddress').val(),
+			password: $('#password').val()
+			};
+	localStorage.removeItem('loginData',JSON.stringify(loginData))
+}
