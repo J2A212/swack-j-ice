cdeb45387f06abf298beaba5627cfe366a3c37c7
s20212029 <s20212029@HCS19071.stdlan.hcs.ac.jp>
2022-11-14 14:35:43 +0900

    少しいじりました

--- src/main/java/servlet/MemberInvitationServlet.java
+++ src/main/java/servlet/MemberInvitationServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.Room;
+import bean.User;
 import exception.SwackException;
 import model.RoomModel;
 
@@ -24,14 +25,6 @@ public class MemberInvitationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
 		try {
 			RoomModel roomModel = new RoomModel();
 			// 名前のリストを受け取る
@@ -47,6 +40,48 @@ public class MemberInvitationServlet extends HttpServlet {
 		} catch (SwackException e) {
 			e.printStackTrace();
 			request.setAttribute("errorMsg", ERR_SYSTEM);
+			return;
+
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+
+		String roomname = request.getParameter("roomname");
+		String createUserId = user.getUserId();
+		// 下二つは仮のデータ
+		boolean directed = false;
+		boolean privated = false;
+		System.out.println("roomname: " + roomname);
+
+
+		String[] invitemembers = request.getParameterValues("join");
+
+		RoomModel roomModel = new RoomModel();
+		
+		
+		try {
+			// 新規ルーム作成
+			String newRoomId=roomModel.newRoom(roomname, createUserId, directed, privated);
+			
+			for (String memberValue: invitemembers) {
+				roomModel.joinRoom(newRoomId, createUserId);
+				System.out.println("invitemember: " + memberValue);
+			}
+
+			session.setAttribute("user", user);
+			response.sendRedirect("main.jsp");
+			return;
+
+		} catch (SwackException e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", ERR_SYSTEM);
+			request.getRequestDispatcher("main.jsp").forward(request, response);
 			return;
 
 		}
